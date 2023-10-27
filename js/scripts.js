c6708e40360d9fc39831379c92ee3ad6869ef34b
amac81 <arnaldocanelas@gmail.com>
2023-10-27 17:21:29 +0100

    js update: note add

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,41 @@
 // seleccionar elementos
 const exportNotesButton = document.querySelector("#export-notes-btn");
-const allNotes = document.querySelector("#notes-container");
+const notesContainer = document.querySelector("#notes-container");
 
+const noteInput = document.querySelector("#note-content");
+const addNoteBtn = document.querySelector("#add-note-btn");
 
 // Funções
+const addNote = () => {
+    const noteObj = {
+        id: generateId(),
+        content: noteInput.value,
+        fixed: false,
+    };
 
+    const noteElement = noteElementCreate(noteObj.id, noteObj.content);
+    notesContainer.appendChild(noteElement);
+}
+
+const generateId = () => {
+    return Math.floor(Math.random() * 5000);
+}
+
+const noteElementCreate = (id, content, fixed) => {
+    const elem = document.createElement("div");
+    elem.classList.add("note");
+    const textArea = document.createElement("textarea");
+    textArea.value = content;
+    textArea.placeholder = "Adicione texto...";
+    elem.appendChild(textArea);
+    
+    return elem;
+};
 
 // Eventos
 
 exportNotesButton.addEventListener("click", (e) => {
     e.preventDefault();
-
-
 });
 
 
@@ -20,10 +44,11 @@ document.addEventListener("click", (elem) => {
     const targetElement = elem.target;
     const parentElement = targetElement.closest("div");
     
-    
-    if(parentElement.classList.contains("note") &&targetElement.classList.contains("bi-pin") ) {
-        console.log(parentElement)
-        parentElement.classList.toggle("fixed");
+    if(parentElement){
+        if(parentElement.classList.contains("note") &&targetElement.classList.contains("bi-pin") ) {
+            console.log(parentElement)
+            parentElement.classList.toggle("fixed");
+        }
     }
 
     //if(parentElement.classList.contains("fixed")) {
@@ -32,3 +57,6 @@ document.addEventListener("click", (elem) => {
     //}
 
 });
+
+
+addNoteBtn.addEventListener("click", () => addNote());
