76e4efb3f613eea8a1631b3d015c128d8b0ad937
amac81 <arnaldocanelas@gmail.com>
2023-10-28 12:49:33 +0100

    Js note change color

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,11 @@ const addNote = () => {
 
     const noteElement = noteElementCreate(noteObj.id, noteObj.content);
     notesContainer.appendChild(noteElement);
-}
+};
 
 const generateId = () => {
     return Math.floor(Math.random() * 5000);
-}
+};
 
 const noteElementCreate = (id, content, fixed) => {
     const elem = document.createElement("div");
@@ -32,8 +32,38 @@ const noteElementCreate = (id, content, fixed) => {
     return elem;
 };
 
-// Eventos
+//https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
+    const hex = x.toString(16)
+    return hex.length === 1 ? '0' + hex : hex
+  }).join('');
 
+const getElementHexBgColor = (elem) => {
+    let elemStyle = window.getComputedStyle(elem,"");
+    let bgColor = elemStyle.getPropertyValue("background-color").trim();
+    bgColor =  bgColor.replace('rgb(', '').replace(')','').split(',');
+
+    let hexBgColor = rgbToHex(parseInt(bgColor[0]), parseInt(bgColor[1]), parseInt(bgColor[2]));
+    return hexBgColor;
+}
+
+const changeNoteColor = (note) => {
+    const availableColors = ["#f8eb75", "#c5f4fa", "#c6ff91", "#ffffcc", "#ffb3ff"];
+   
+    const bgColor = getElementHexBgColor(note).toString();
+    let colorIndex = availableColors.indexOf(bgColor);
+   
+    if(colorIndex >= 0 && colorIndex < availableColors.length){
+        colorIndex ++;
+
+        if(colorIndex >= availableColors.length){
+            colorIndex = 0;
+        }
+        note.style.backgroundColor = availableColors[colorIndex];
+    }
+};
+
+// Eventos
 exportNotesButton.addEventListener("click", (e) => {
     e.preventDefault();
 });
@@ -44,17 +74,19 @@ document.addEventListener("click", (elem) => {
     const targetElement = elem.target;
     const parentElement = targetElement.closest("div");
     
+    //bi-pin button
     if(parentElement){
         if(parentElement.classList.contains("note") &&targetElement.classList.contains("bi-pin") ) {
-            console.log(parentElement)
             parentElement.classList.toggle("fixed");
         }
     }
 
-    //if(parentElement.classList.contains("fixed")) {
-        //toggleNote();
-        
-    //}
+  //bi-palette button
+  if(parentElement){
+    if(parentElement.classList.contains("note") &&targetElement.classList.contains("bi-palette") ) {
+        changeNoteColor(parentElement);
+    }
+}
 
 });
 
